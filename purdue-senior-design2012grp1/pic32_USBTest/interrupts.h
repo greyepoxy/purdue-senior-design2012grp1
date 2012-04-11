6214fdd98c7059fdc06c1c2ee4f5550c014eaa5c
jhuffake@purdue.edu <jhuffake@purdue.edu@cb290218-39de-c211-9623-2a464aa4ce5e>
2012-04-11 02:46:26 +0000

    usb test program

---
+++ purdue-senior-design2012grp1/pic32_USBTest/interrupts.h
@@ -0,0 +1,13 @@
+/*
+ * File:   interrupts.h
+ * Author: Justin
+ *
+ * Created on February 22, 2012, 5:36 PM
+ */
+
+#ifndef interrupts_H
+#define interrupts_H
+
+volatile unsigned int LEDOnFlag;
+
+#endif
